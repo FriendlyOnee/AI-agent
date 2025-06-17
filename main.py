1d66f464ed6be9307c8b7cb482d2d90b27239ea1
FriendlyOnee <danila.vadimovich@gmail.com>
2025-06-17 14:48:06 +0000

    Optional verbose flag added

--- main.py
+++ main.py
@@ -2,19 +2,36 @@ import os
 import sys
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
 
 client = genai.Client(api_key=api_key)
 
-if len(sys.argv) < 1:
+verbose = "--verbose" in sys.argv
+
+if verbose:
+    sys.argv.remove("--verbose")
+
+if len(sys.argv) < 2:
     raise Exception("Prompt wasn't provided")
 
+user_prompt = sys.argv[1]
+
+messages = [
+    types.Content(role="user", parts=[types.Part(text=user_prompt)]),
+]
+
 response = client.models.generate_content(
-    model="gemini-2.0-flash-001", contents=sys.argv[1]
+    model="gemini-2.0-flash-001", contents=messages
 )
 
+if verbose:
+    print(f"User prompt: {user_prompt}")
+
 print(response.text)
-print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+
+if verbose:
+    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
